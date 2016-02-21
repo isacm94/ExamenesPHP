6b6f31031a3d11c9b961369c659624aeb14f5f4a
Isa Calvo <isacm94@gmail.com>
2016-02-21 19:00:33 +0100

    21/02/2016, Ejercicio 3 Exámen php 2014 soap

--- 2014/Ejercicio3/application/controllers/Ctrl_index.php
+++ 2014/Ejercicio3/application/controllers/Ctrl_index.php
@@ -11,21 +11,26 @@ class Ctrl_index extends CI_Controller {
     public function index() {
         $client = new SoapClient("http://footballpool.dataaccess.eu/data/info.wso?wsdl");
 
-        echo "<h1>Funciones Disponibles</h1>";
-	echo "<pre>".print_r($client->__getFunctions(),true)."</pre>";
-	
-	
-	echo "<h1>Tipos Disponibles</h1>";
-	echo "<pre>".print_r($client->__getTypes(),true)."</pre>";
+//        echo "<h1>Funciones Disponibles</h1>";
+//	echo "<pre>".print_r($client->__getFunctions(),true)."</pre>";
+//	
+//	
+//	echo "<h1>Tipos Disponibles</h1>";
+//	echo "<pre>".print_r($client->__getTypes(),true)."</pre>";
         
-        $result = $client->AllPlayersWithYellowCards(true);
+        $result = $client->AllPlayersWithYellowCards(array('bSortedByName' => true, 'bSortedByYellowCards' => false));
 
         // Note that $array contains the result of the traversed object structure
         //$entrenadores = $result->CoachesResult->tCoaches;
-        $jugadores = $result->tPlayerNames;
-        echo '<pre>';
-        print_r($jugadores);
-        echo '</pre>';
+        $jugadores = $result->AllPlayersWithYellowCardsResult->tPlayersWithCards;
+        
+        echo '<h1>Jugadores con tarjetas amarillas</h1>';
+        foreach ($jugadores as $key => $value) {
+            echo  $value->sName.' - '.$value->iYellowCards.'<br>';
+        }
+//        echo '<pre>';
+//        print_r($jugadores);
+//        echo '</pre>';
         
         
     }
